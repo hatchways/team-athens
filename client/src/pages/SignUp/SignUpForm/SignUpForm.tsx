5494fec13472b7e0de183cd52733d397b6c73567
Christian Barnabé <62568487+christian-barnabe-chabi@users.noreply.github.com>
2021-06-01 23:20:50 +0000

    'login component for demo user created & added on login/sign up pages'

--- client/src/pages/SignUp/SignUpForm/SignUpForm.tsx
+++ client/src/pages/SignUp/SignUpForm/SignUpForm.tsx
@@ -6,6 +6,7 @@ import * as Yup from 'yup';
 import Typography from '@material-ui/core/Typography';
 import useStyles from './useStyles';
 import { CircularProgress } from '@material-ui/core';
+import LogInWithDemoUser from '../../../components/LoginWithDemoUser/LogInWithDemoUser';
 
 interface Props {
   handleSubmit: (
@@ -105,6 +106,7 @@ const SignUpForm = ({ handleSubmit }: Props): JSX.Element => {
             <Button type="submit" size="large" variant="contained" color="primary" className={classes.submit}>
               {isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'Create an account'}
             </Button>
+            <LogInWithDemoUser></LogInWithDemoUser>
           </Box>
         </form>
       )}
