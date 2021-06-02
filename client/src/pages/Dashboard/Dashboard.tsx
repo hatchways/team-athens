570c15f3e509773843e50f9c9e71dbafb322039f
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2021-06-02 04:34:33 -0400

    basic nav-bar completed

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -8,6 +8,19 @@ import { useHistory } from 'react-router-dom';
 import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
 import { useEffect } from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Paper } from '@material-ui/core';
+import Image from 'material-ui-image';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import LogoImage from './../../Images/logo.png';
+import Badge from '@material-ui/core/Badge';
+import MailIcon from '@material-ui/icons/Mail';
 export default function Dashboard(): JSX.Element {
   const classes = useStyles();
 
@@ -30,9 +43,36 @@ export default function Dashboard(): JSX.Element {
   return (
     <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
       <CssBaseline />
-      <Grid item className={classes.drawerWrapper}>
-        <ChatSideBanner loggedInUser={loggedInUser} />
-      </Grid>
+      <AppBar position="static">
+        <Toolbar className={classes.customizeToolbar}>
+          <Typography className={classes.logo}>Deals Mate</Typography>
+          <Box className={classes.headerButtonGroup}>
+            <Button color="inherit">Shopping List</Button>
+            <Button color="inherit">Friends</Button>
+            <Button color="inherit">
+              <Badge variant="dot" color="secondary" invisible={false}>
+                Notifications
+              </Badge>
+            </Button>
+          </Box>
+          <Box className={classes.userPanel}>
+            <Avatar alt="Profile Image" />
+            <Typography variant="h5" className={classes.userText}>
+              {loggedInUser.username}
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {/* <Paper>
+        <Image src={LogoImage} />
+      </Paper> */}
+      {/* <Grid item className={classes.drawerWrapper}> */}
+      {/* <ChatSideBanner loggedInUser={loggedInUser} /> */}
+      {/* </Grid> */}
+
+      {/* <Paper variant="outlined">
+        <Image src={LogoImage} className={classes.logo} />
+      </Paper> */}
     </Grid>
   );
 }
--- client/src/pages/Dashboard/useStyles.ts
+++ client/src/pages/Dashboard/useStyles.ts
@@ -4,17 +4,44 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: '100vh',
+    // minHeight: '100vh',
     '& .MuiInput-underline:before': {
       borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
     },
+    flexGrow: 1,
+  },
+  customizeToolbar: {
+    maxHeight: 36,
+    width: '90%',
+    margin: '0 auto',
   },
   dashboard: { backgroundColor: '#FFFFFF' },
   drawerWrapper: {
     width: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      width: '300px',
-    },
+    // [theme.breakpoints.up('md')]: {
+    //   width: '300px',
+    // },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  logo: {
+    flexGrow: 1,
+  },
+  userPanel: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  headerButtonGroup: {
+    marginRight: '4rem',
+  },
+  userText: {
+    fontWeight: 700,
+    paddingLeft: '1rem',
+    fontSize: 16,
   },
 }));
 
