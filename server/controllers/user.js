5fa838edd42d36d2bd77431cb41d5914ded72aa8
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2021-06-10 06:39:59 -0400

    fixed style and nesting issues

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -26,28 +26,31 @@ exports.searchUsers = asyncHandler(async (req, res, next) => {
 // @desc Follow a user
 // @access Private
 exports.followUser = asyncHandler(async (req, res, next) => {
-  const current_username = req.body.username;
-  const other_username = req.params.username;
+  const currentUsername = req.body.username;
+  const otherUsername = req.params.username;
 
-  if (other_username !== current_username) {
-    try {
-      const user = await User.findOne({ username: other_username });
-      const currentuser = await User.findOne({ username: current_username });
-
-      // check if ur already following this user
-      if (!user.followers.includes(currentuser._id)) {
-        await user.updateOne({ $push: { followers: currentuser._id } });
-        await currentuser.updateOne({ $push: { followings: user._id } });
-
-        res.status(200).json(`${currentuser.username} is now following ${user.username}`)
-      } else {
-        res.status(403).json("you already follow this user");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
+  // early exit
+  if (otherUsername === currentUsername) {
     res.status(403).json("you can't follow yourself");
+    next();
+  }
+  try {
+    const user = await User.findOne({ username: otherUsername });
+    const currentuser = await User.findOne({ username: currentUsername });
+
+    // check if ur already following this user
+    if (user.followers.includes(currentuser._id)) {
+      res.status(403).json("you already follow this user");
+      next();
+    }
+    // follow
+    await user.updateOne({ $push: { followers: currentuser._id } });
+    await currentuser.updateOne({ $push: { followings: user._id } });
+
+    res.status(200).json(`${currentuser.username} is now following ${user.username}`)
+
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -55,28 +58,29 @@ exports.followUser = asyncHandler(async (req, res, next) => {
 // @desc Unfollow a user
 // @access Private
 exports.unfollowUser = asyncHandler(async (req, res, next) => {
-  const current_username = req.body.username;
-  const other_username = req.params.username;
+  const currentUsername = req.body.username;
+  const otherUsername = req.params.username;
 
-  if (other_username !== current_username) {
-    try {
-      const user = await User.findOne({ username: other_username });
-      const currentuser = await User.findOne({ username: current_username });
-
-
-      if (user.followers.includes(currentuser._id)) {
-        await user.updateOne({ $pull: { followers: currentuser._id } });
-        await currentuser.updateOne({ $pull: { followings: user._id } });
-
-        res.status(200).json(`${currentuser.username} has unfollowed ${user.username}`)
-      } else {
-        res.status(403).json("you are not following this user");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
+  if (otherUsername === currentUsername) {
     res.status(403).json("you can't unfollow yourself");
+    next();
+  }
+
+  try {
+    const user = await User.findOne({ username: otherUsername });
+    const currentuser = await User.findOne({ username: currentUsername });
+
+    if (!user.followers.includes(currentuser._id)) {
+      res.status(403).json("you are not following this user");
+      next();
+    }
+
+    await user.updateOne({ $pull: { followers: currentuser._id } });
+    await currentuser.updateOne({ $pull: { followings: user._id } });
+
+    res.status(200).json(`${currentuser.username} has unfollowed ${user.username}`)
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
