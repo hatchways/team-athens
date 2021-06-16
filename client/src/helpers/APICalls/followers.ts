77da3518dc40320f2b314d85d3c881494d58688a
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2021-06-16 08:52:25 -0400

    changed frontend calls to match backend changes

--- client/src/helpers/APICalls/followers.ts
+++ client/src/helpers/APICalls/followers.ts
@@ -1,54 +1,50 @@
 import { AuthApiData } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
-const getFollowings = async (username: any): Promise<AuthApiData> => {
+const getFollowings = async (): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
   };
-  return await fetch(`/users/${username}/followings`, fetchOptions)
+  return await fetch(`/users/followings`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
     }));
 };
 
-const getFollowers = async (username: any): Promise<AuthApiData> => {
+const getFollowers = async (): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
   };
-  return await fetch(`/users/${username}/followers`, fetchOptions)
+  return await fetch(`/users/followers`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
     }));
 };
 
-const getSuggestions = async (username: any): Promise<AuthApiData> => {
+const getSuggestions = async (): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
   };
-  return await fetch(`/users/${username}/followSugestions`, fetchOptions)
+  return await fetch(`/users/followSugestions`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
     }));
 };
 
-const followUser = async (myUsername: any, theirUserName: any): Promise<AuthApiData> => {
-  const body = {
-    username: myUsername,
-  };
+const followUser = async (theirUserName: any): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
-    body: JSON.stringify(body),
   };
   return await fetch(`/users/${theirUserName}/follow`, fetchOptions)
     .then((res) => res.json())
@@ -57,15 +53,11 @@ const followUser = async (myUsername: any, theirUserName: any): Promise<AuthApiD
     }));
 };
 
-const unfollowUser = async (myUsername: any, theirUserName: any): Promise<AuthApiData> => {
-  const body = {
-    username: myUsername,
-  };
+const unfollowUser = async (theirUserName: any): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
-    body: JSON.stringify(body),
   };
   return await fetch(`/users/${theirUserName}/unfollow`, fetchOptions)
     .then((res) => res.json())
